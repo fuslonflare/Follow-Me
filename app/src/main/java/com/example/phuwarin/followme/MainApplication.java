1fb5f0004722164fd0a35a05459a317a9d3746d4
Phuwarin <pang-pond_08@hotmail.com>
2017-06-28 21:52:52 +0700

    Initial Fabric

--- app/src/main/java/com/example/phuwarin/followme/MainApplication.java
+++ app/src/main/java/com/example/phuwarin/followme/MainApplication.java
@@ -2,7 +2,10 @@ package com.example.phuwarin.followme;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
 import com.example.phuwarin.followme.manager.ContextBuilder;
+
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by Phuwarin on 4/5/2017.
@@ -15,6 +18,7 @@ public class MainApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
 
         ContextBuilder.getInstance().init(getApplicationContext());
     }
